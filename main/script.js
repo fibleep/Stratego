05638364312d93fe77dc626f42954fd2ccb2200c
fibleep <filipn924@gmail.com>
2022-02-19 15:29:07 +0100

    html structure + responsiveness + start of javascript
    website ready to be shaped into whatever needs to be done.

---
+++ main/script.js
@@ -0,0 +1,9 @@
+function showOptions(){
+    let list = document.getElementById("mobile-list");
+    if(list.style.display=="none"){
+        list.style.display="flex";
+    }
+    else{
+        list.style.display="none";
+    }
+}
