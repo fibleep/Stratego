b080fb80eda6e76908da204124df8c219d4d1c18
fibleep <filipn924@gmail.com>
2022-02-25 13:36:47 +0100

    rules page + a bit of js

--- main/script.js
+++ main/script.js
@@ -7,3 +7,18 @@ function showOptions(){
         list.style.display="none";
     }
 }
+function showCard(card){
+    console.log(card);
+    const cards=['marshal','spy','general','colonel','major','captain','lieutenant','sergeant','flag','bomb','scout','miner'];
+    for(let i=0;i<cards.length;i++){
+        isVisible(String(cards[i]));
+    }
+    document.getElementById("descriptions").style.display="block";
+    document.getElementById("main").style.display="none"
+    document.getElementById(card).style.display="block";
+}
+function isVisible(card){
+    if (document.getElementById(card).style.display=="block"){
+        document.getElementById(card).style.display="none";
+    }
+}
